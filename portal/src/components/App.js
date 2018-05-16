93498733f11cfd335328552e01f2e0eabd234898
Guinsly Mondésir <guinslym@gmail.com>
2018-05-16 01:33:57 -0400

    finish part 1

--- portal/src/components/App.js
+++ portal/src/components/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import News from './News/News';
+import { concat, sortBy, map, sample , shuffle} from 'lodash'
+// import concat from 'lodash/concat';
 
 class App extends Component {
+  constructor(props){
+  	super(props);
+  	this.state = {
+      newsCategory : ['top-headlines?sources=bbc-news', 'top-headlines?country=de&category=business', 'top-headlines?q=trump', 'everything?q=apple&from=2018-05-15&to=2018-05-15&sortBy=popularity','everything?q=reactjs']
+    };
+
+  }
+
+randomTopics(){
+  console.log(this.state.newsCategory);
+  const sh = shuffle(this.state.newsCategory);
+  return shuffle(this.state.newsCategory);
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to News Feed API</h1>
         </header>
-        <News />
+        <News topic={shuffle(this.state.newsCategory)[0]}/>
       </div>
     );
   }
--- portal/src/components/News/News.js
+++ portal/src/components/News/News.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import NewSingle from './NewSingle'
+import { concat, sortBy, map, sample , shuffle} from 'lodash'
 
 export default class News extends Component {
 
@@ -9,9 +10,14 @@ constructor(props){
     news: [],
   };
 }
+// _.shuffle([1, 2, 3, 4]);
+// var other = _.concat(array, 2, [3], [[4]]);
+
 
 componentDidMount() {
-  const url='https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=d5cf45043cd34b59b432df10e3cef274';
+  const url= `https://newsapi.org/v2/${this.props.topic}&apiKey=d5cf45043cd34b59b432df10e3cef274`;
+
+	console.log(url)
 
   fetch(url)
   .then((response)=> {
